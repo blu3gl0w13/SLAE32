aa9611984edcacb4a1d09fb8b3f1c8026d230bc6
Blu3gl0w13 <blu3gl0w13@gmail.com>
2016-09-28 16:13:18 -0400

    Working encryption and decryption, need to execute now for assignment 7

--- assignment-7/shellcode-crypter.py
+++ assignment-7/shellcode-crypter.py
@@ -12,9 +12,10 @@
 # Imports
 
 from Crypto.Cipher import AES
-from Crypto.Cipher import ARC4
 import sys
 import argparse
+import os
+import hashlib
 
 
 #---------------------------------------
@@ -23,39 +24,20 @@ import argparse
 #
 #--------------------------------------
 
-def aesCrypter(key, initVector, shellcode):
-  encrypterObject = AES.new(key, AES.MODE_CBC, initVector)
+def aesCrypter(key, shellcode):
+  salt = os.urandom(16)
+  initVector = os.urandom(16)
+  hashedKey = hashlib.sha256(key + salt).digest()
+  mode = AES.MODE_CBC
+  encrypterObject = AES.new(hashedKey, mode, initVector)
   messageToEncrypt = shellcode
-  cipherText = encrypterObject.encrypt(messageToEncrypt)
+  cipherText = initVector + salt + encrypterObject.encrypt(messageToEncrypt)
   print "\n\n[+] RAW AES Encrypted Shellcode: \n%s" % cipherText.encode('hex')
-  encryptedShellcode = ''
-  encryptedShellcode2 = ''
-
-  for i in bytearray(cipherText):
-    encryptedShellcode += '\\x%02x' % i
-    encryptedShellcode2 += '0x%02x,' % i
   print "\nShellcode Length: %d" % len(cipherText)
   print "\nKey: %s" % key
-  print "\nIV: %s" % initVector
-  print "\n[+] AES Encrypted Shellcode: \n" + encryptedShellcode
-  print "\n[+] AES Encrypted Shellcode2: \n" + encryptedShellcode2[:(len(encryptedShellcode2) - 1):]
-  sys.exit(0)
-
-def rc4Crypter(key, shellcode):
-  encrypterObject = ARC4.new(key)
-  messageToEncrypt = shellcode
-  cipherText = encrypterObject.encrypt(messageToEncrypt)
-  print "\n\n[+] RAW RC4 Encrypted Shellcode: \n%s" % cipherText.encode('hex')
-  encryptedShellcode = ''
-  encryptedShellcode2 = ''
-
-  for i in bytearray(cipherText):
-    encryptedShellcode += '\\x%02x' % i
-    encryptedShellcode2 += '0x%02x,' % i
-  print "\nShellcode Length: %d" % len(cipherText)
-  print "\nKey: %s" % key
-  print "\nRC4 Encrypted Shellcode: \n" + encryptedShellcode
-  print "\nRC4 Encrypted Shellcode2: \n" + encryptedShellcode2[:(len(encryptedShellcode2) - 1):]
+  print "\nHashedkey: %s Len: %d" % (hashedKey.encode('hex'), len(hashedKey))
+  print "\nSalt: %s Len: %d" % (salt.encode('hex'), len(salt))
+  print "\nIV: %s Len: %d\n\n" % (initVector.encode('hex'), len(initVector))
   sys.exit(0)
 
 def main():
@@ -64,42 +46,16 @@ def main():
   parser = argparse.ArgumentParser()
   parser.add_argument("-s", "--shellcode", help="Shellcode to encrypt", dest='shellcode', required=True)
   parser.add_argument('-k', '--key', help='AES key to use for encryption', dest='key', required=True)
-  parser.add_argument('-c', '--cipher', help='Cipher to use for encryption', choices=['rc4','aes','RC4','AES'], dest='cipher', required=True)
-  parser.add_argument('-iv', '--init-vector', help='AES IV to use for encryption (AES only)', dest='initVector')
   options = parser.parse_args()
 
   # Prepare some objects
 
   key = options.key
-  initVector = options.initVector
   shellcode = options.shellcode
+  while (len(shellcode) % 16 !=0):
+    shellcode += "\x90"
 
-  if options.cipher == 'aes' or options.cipher == 'AES':
-    # pad shellcode with nops until it is in chunks of 16
-    while (len(shellcode) % 16 !=0):
-      shellcode += '\\x90'
-
-    if initVector == 'None':
-      print "\n[-] Error! IV option not set. Exiting..."
-      parser.print_help()
-      sys.exit(1)
-    if len(initVector) != 16:
-      print "\n[-] Error! IV is not 16 bits long. Exiting..."
-      sys.exit(1)
-    if (len(key) != 16) and (len(key) != 24) and (len(key) != 32):
-      print "\n[-] Error! AES Keys must be 16, or 24, or 32 bits. Exiting..."
-      sys.exit(1)
-
-    aesCrypter(key, initVector, shellcode)
-
-
-  elif options.cipher == 'rc4' or options.cipher == 'RC4':
-    rc4Crypter(key,shellcode)
-
-
-  else:
-    print "[-] Error! Not sure how you got here. Exiting..."
-    sys.exit(1)
+  aesCrypter(key, shellcode)
 
 
 if __name__ != main:
--- assignment-7/shellcode-decrypter.py
+++ assignment-7/shellcode-decrypter.py
@@ -12,9 +12,10 @@
 # Imports
 
 from Crypto.Cipher import AES
-from Crypto.Cipher import ARC4
 import sys
 import argparse
+import os
+import hashlib
 
 
 #---------------------------------------
@@ -23,39 +24,14 @@ import argparse
 #
 #--------------------------------------
 
-def aesDecrypter(key, initVector, shellcode):
-  decrypterObject = AES.new(key, AES.MODE_CBC, initVector)
+def aesDecrypter(key, IV, shellcode, salt):
+  hashedKey = hashlib.sha256(key + salt).digest()
+  mode = AES.MODE_CBC
+  initVector = IV
+  decrypterObject = AES.new(hashedKey, AES.MODE_CBC, initVector)
   messageToDecrypt = shellcode
   clearText = decrypterObject.decrypt(messageToDecrypt)
-  print "\n\n[+] RAW AES Decrypted Shellcode: \n%s" % clearText.encode('hex')
-  decryptedShellcode = ''
-  decryptedShellcode2 = ''
-
-  for i in bytearray(clearText):
-    decryptedShellcode += '\\x%02x' % i
-    decryptedShellcode2 += '0x%02x,' % i
-  print "\nShellcode Length: %d" % len(clearText)
-  print "\nKey: %s" % key
-  print "\nIV: %s" % initVector
-  print "\n[+] AES Decrypted Shellcode: \n" + decryptedShellcode
-  print "\n[+] AES Decrypted Shellcode2: \n" + decryptedShellcode2[:(len(decryptedShellcode2) - 1):]
-  sys.exit(0)
-
-def rc4Decrypter(key, shellcode):
-  decrypterObject = ARC4.new(key)
-  messageToDecrypt = shellcode
-  clearText = encrypterObject.decrypt(messageToDecrypt)
-  print "\n\n[+] RAW RC4 Encrypted Shellcode: \n%s" % clearText.encode('hex')
-  decryptedShellcode = ''
-  decryptedShellcode2 = ''
-
-  for i in bytearray(cipherText):
-    decryptedShellcode += '\\x%02x' % i
-    decryptedShellcode2 += '0x%02x,' % i
-  print "\nShellcode Length: %d" % len(clearText)
-  print "\nKey: %s" % key
-  print "\nRC4 Encrypted Shellcode: \n" + decryptedShellcode
-  print "\nRC4 Encrypted Shellcode2: \n" + decryptedShellcode2[:(len(decryptedShellcode2) - 1):]
+  print "\n\n[+] RAW AES Decrypted Shellcode (non hex encoded): \n\"%s\"\n\n" % clearText
   sys.exit(0)
 
 def main():
@@ -64,43 +40,15 @@ def main():
   parser = argparse.ArgumentParser()
   parser.add_argument("-s", "--shellcode", help="Shellcode to decrypt", dest='shellcode', required=True)
   parser.add_argument('-k', '--key', help='Key to use for decryption', dest='key', required=True)
-  parser.add_argument('-c', '--cipher', help='Cipher to use for decryption', choices=['rc4','aes','RC4','AES'], dest='cipher', required=True)
-  parser.add_argument('-iv', '--init-vector', help='AES IV to use for decryption (AES only)', dest='initVector')
   options = parser.parse_args()
 
   # Prepare some objects
-
+  shellcode = (options.shellcode).decode('hex')[32::]
+  IV = (options.shellcode).decode('hex')[:16]
+  salt = (options.shellcode).decode('hex')[16:32]
   key = options.key
-  initVector = options.initVector
-  shellcode = options.shellcode
 
-  if options.cipher == 'aes' or options.cipher == 'AES':
-    # pad shellcode with nops until it is in chunks of 16
-    #while (len(bytearray(shellcode)) % 16 !=0):
-    #  shellcode += '\\x90'
-
-    if initVector == 'None':
-      print "\n[-] Error! IV option not set. Exiting..."
-      parser.print_help()
-      sys.exit(1)
-    if len(initVector) != 16:
-      print "\n[-] Error! IV is not 16 bits long. Exiting..."
-      sys.exit(1)
-    if (len(key) != 16) and (len(key) != 24) and (len(key) != 32):
-      print "\n[-] Error! AES Keys must be 16, or 24, or 32 bits. Exiting..."
-      sys.exit(1)
-
-    aesDecrypter(key, initVector, shellcode)
-
-
-  elif options.cipher == 'rc4' or options.cipher == 'RC4':
-    rc4Decrypter(key,shellcode)
-
-
-  else:
-    print "[-] Error! Not sure how you got here. Exiting..."
-    sys.exit(1)
-
+  aesDecrypter(key, IV, shellcode, salt)
 
 if __name__ != main:
   main()
